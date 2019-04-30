cf1b150b68d0d4bea59c6212093dde3533fc00ad
K-H-Ismail <khalfaoui.ismail@hotmail.com>
2019-05-01 01:27:38 +0200

    ajout de la conf serveur et de quelques commandes pour l'acquisition de vidéos

--- desktop-record.sh
+++ desktop-record.sh
@@ -1,2 +1,22 @@
-ffmpeg -f x11grab -s 1366x768 -r 30 -i :0.0 -f alsa -i pulse -vcodec libx264 ~/test.mp4
+# to record a video(any format) film use:
+ffmpeg -i ~/dwhelper/film.mp4  -c:v h264 -c:a aac  -strict -2 -f flv "rtmp://localhost:1935/live/" 
+
+
+# to record pc screen use:
+ffmpeg -f x11grab -s 1280x720 -r 30 -i :0.0 \
+-f alsa -ac 2 -i pulse -vcodec libx264 \
+-s 1280x720 -strict -2 -acodec aac \
+-ab 128k -ar 44100 -threads 0 \
+-f flv "rtmp://localhost:1935/live"
+
+# to find out the resolution of the client
+`xdpyinfo | grep 'dimensions:'|awk '{print $2}'` 
+
+
+# webcam 
+
+ffmpeg -f video4linux2 -i /dev/video0 -codec:v libx264 -pix_fmt yuv420p -f flv "rtmp://localhost:1935/live"
+
+# webcam + sound
+ffmpeg -f v4l2 -i /dev/video0 -f alsa -i pulse -profile:v high -pix_fmt yuvj420p -level:v 4.1 -preset ultrafast -tune zerolatency -vcodec libx264 -r 10 -b:v 512k -acodec aac -strict -2 -ac 2 -ab 32k -ar 44100 -f mpegts -flush_packets 0 -f flv "rtmp://localhost:1935/live"
 
