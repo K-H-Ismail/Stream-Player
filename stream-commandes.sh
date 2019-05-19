723c40b3437a3e1dff54a4981cb7a47d395df8a6
K-H-Ismail <khalfaoui.ismail@hotmail.com>
2019-05-19 16:42:53 +0200

    Update stream-commandes.sh

--- stream-commandes.sh
+++ stream-commandes.sh
@@ -20,4 +20,4 @@ ffmpeg -f v4l2 -i /dev/video0 -f alsa -i pulse -profile:v high -pix_fmt yuvj420p
 
 # to record all of that with webcam overlay in bottom
 
-ffmpeg -f alsa -i pulse -f x11grab -s `xdpyinfo | grep 'dimensions:'|awk '{print $2}'`  -r 30 -i :0.0+0,0 -vf "movie=/dev/video0:f=video4linux2, scale=240:-1, fps, setpts=PTS-STARTPTS [movie]; [in][movie] overlay=main_w-overlay_w-2:main_h-overlay_h-2 [out]" -vcodec libx264 -crf 20 -preset veryfast -minrate 150k -maxrate 500k -s 960x540 -acodec aac -ar 44100 -ab 96000 -threads 0 -f flv - | tee name.flv | ffmpeg -i - -codec copy -f flv -metadata streamName=livestream "rtmp://localhost:1935/live"
+ffmpeg -f alsa -i pulse -f x11grab -s "`xdpyinfo | grep 'dimensions:'|awk '{print $2}'`"  -r 30 -i :0.0+0,0 -vf "movie=/dev/video0:f=video4linux2, scale=240:-1, fps, setpts=PTS-STARTPTS [movie]; [in][movie] overlay=main_w-overlay_w-2:main_h-overlay_h-2 [out]" -vcodec libx264 -crf 20 -preset veryfast -minrate 150k -maxrate 500k -s 960x540 -acodec aac -ar 44100 -ab 96000 -threads 0 -f flv - | tee name.flv | ffmpeg -i - -codec copy -f flv -metadata streamName=livestream "rtmp://localhost:1935/live"
